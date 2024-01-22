45f2e0f999a477344cbc6ab32fae461b5fc50d41
virtuallyunknown <dechobarca@gmail.com>
2024-01-22 06:59:50 +0200

    fix(eslint-client): apply linting and fixes

--- packages/eslint-client/src/components/ErrorLevel.tsx
+++ packages/eslint-client/src/components/ErrorLevel.tsx
@@ -3,7 +3,7 @@ import { clsx } from 'clsx';
 export const ErrorLevel = ({ level }: { level: 0 | 1 | 2 }) => {
     return (
         <div className={clsx(
-            'mt-1 h-3 w-3 rounded',
+            'mt-1 size-3 rounded',
             level === 0 && 'bg-gray-300',
             level === 1 && 'bg-yellow-300',
             level === 2 && 'bg-red-300',
