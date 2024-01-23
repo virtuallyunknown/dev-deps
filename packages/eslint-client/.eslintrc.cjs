f7be3d76675bd4c3f3c9bb99e4b537a29f0f908b
virtuallyunknown <dechobarca@gmail.com>
2024-01-23 03:22:11 +0200

    feat(eslint-client): add linting configuration

--- packages/eslint-client/.eslintrc.cjs
+++ packages/eslint-client/.eslintrc.cjs
@@ -1,10 +1,26 @@
+// module.exports = {
+//     root: true,
+//     extends: ["plugin:tailwindcss/recommended"],
+//     overrides: [
+//         {
+//             files: ['*.ts', '*.tsx', '*.js'],
+//             parser: '@typescript-eslint/parser',
+//         },
+//     ],
+// };  
+
 module.exports = {
     root: true,
-    extends: ["plugin:tailwindcss/recommended"],
-    overrides: [
-        {
-            files: ['*.ts', '*.tsx', '*.js'],
-            parser: '@typescript-eslint/parser',
-        },
+    extends: [
+        '@virtuallyunknown/eslint-config',
+        '@virtuallyunknown/eslint-config/react',
+        '@virtuallyunknown/eslint-config/stylistic',
+        "plugin:tailwindcss/recommended"
     ],
-};  
+    parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+    ignorePatterns: ['*.*', '!src/**/*'],
+};
+
