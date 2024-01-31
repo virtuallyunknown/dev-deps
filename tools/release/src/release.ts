fef0615ac455f488e6f25f7cb906478182b238c8
virtuallyunknown <dechobarca@gmail.com>
2024-01-31 02:24:18 +0200

    fix(release): accidentally altered the release script, revert to original

--- tools/release/src/release.ts
+++ tools/release/src/release.ts
@@ -18,8 +18,8 @@ await generateChangelog({
     dryRun: dryRun,
     repo: 'dev-deps',
     owner: 'virtuallyunknown',
-    from: '0.0.0',
-    to: '0.0.1',
+    from: currentVersion,
+    to: newVersion,
 });
 
 await releasePublish({
